4ba67e152814a7b8b18a51d53789f252e991d7ca
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-01-29 17:09:00 -0800

    Sync with ports tree version
    Sync with ports tree version

--- winesteam.sh
+++ winesteam.sh
@@ -6,4 +6,4 @@
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "dxgi" /t "REG_SZ" /d "builtin" /f
 # WINEPREFIX=$HOME/.wine_steam winetricks -q nocrashdialog
 
-DXVK_HUD=full WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=0 WINEDEBUG=-all WINEPREFIX=$HOME/.wine_steam /usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-in-process-gpu -cef-disable-gpu-compositing -cef-single-process
+DXVK_HUD=full WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1 WINEDEBUG=-all WINEPREFIX=$HOME/.wine_steam /usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-in-process-gpu -cef-disable-gpu-compositing -cef-single-process
