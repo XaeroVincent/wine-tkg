57ecd1e77014e109a3f3c2e03c72afaf696561a4
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-02-08 21:59:47 -0800

    Update to Wine 11.2
    Update to Wine 11.2 and add cherry picked Wine Staging patches to fix Adobe installers and optionally hide Wine exports

--- winesteam.sh
+++ winesteam.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+export PULSE_LATENCY_MSEC=60
+
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "d3d11" /t "REG_SZ" /d "builtin" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "dxgi" /t "REG_SZ" /d "builtin" /f
-# WINEPREFIX=$HOME/.wine_steam winetricks -q nocrashdialog
+# WINEPREFIX=$HOME/.wine_steam winetricks -q corefonts nocrashdialog
 
-DXVK_HUD=full WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1 WINEDEBUG=-all WINEPREFIX=$HOME/.wine_steam /usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-in-process-gpu -cef-disable-gpu-compositing -cef-single-process
+DXVK_HUD=full WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1 WINEDEBUG=-all WINEPREFIX=$HOME/.wine_steam /usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-in-process-gpu -cef-disable-gpu
