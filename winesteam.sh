df97e534655b991d783c8ece02b243bcfc33e165
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-04-08 08:07:28 -0700

    Update script
    Update script

--- winesteam.sh
+++ winesteam.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-export PULSE_LATENCY_MSEC=60
-
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "d3d11" /t "REG_SZ" /d "builtin" /f
 # WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "dxgi" /t "REG_SZ" /d "builtin" /f
 # WINEPREFIX=$HOME/.wine_steam winetricks -q corefonts nocrashdialog
 
-WINE_LARGE_ADDRESS_AWARE=1 WINEDEBUG="-all" WINEPREFIX=$HOME/.wine_steam /usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-disable-seccomp-sandbox -no-cef-sandbox -cef-disable-gpu -cef-disable-hang-timeouts -console
+export WINEDEBUG="-all"
+export PULSE_LATENCY_MSEC=60
+export WINE_LARGE_ADDRESS_AWARE=1
+export WINEPREFIX=$HOME/.wine_steam
+export WINE_D3D_CONFIG="renderer=vulkan"
+
+/usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -cef-disable-sandbox -cef-disable-seccomp-sandbox -cef-disable-hang-timeouts -cef-disable-gpu -console
